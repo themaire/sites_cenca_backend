667bb95937d902509c8a7bc7bb433c69c21d9008
themaire <n.elie@me.com>
2024-08-27 21:49:18 +0000

    poursuite de lecriture des routes get

--- node_base_sites.js
+++ node_base_sites.js
@@ -25,7 +25,7 @@ pool.connect().then(() => {
 // The Pool is ready to serve various query depending
 
 // Functions
-function joinQuery(select, from, where){
+function joinQuery(select, from, where = ''){
   const query = select + from + where;
 
   // console.log(query);
@@ -38,8 +38,10 @@ async function siteResearch(pool, param, callback){
     if (RESULTS.rows.length === 0){
         callback("Pas de documents trouvés.", [])
     } else if (RESULTS.rows.length > 0 ){
-        console.log("On a des résultats.");
+        console.log(" ");
+        console.log("Résultats depuis la fonction siteResearch()");
         console.log("Params : ", param);
+        console.log(RESULTS.rows.length + " résultats.");
         callback(param["message"], RESULTS.rows);
     } else callback(param["message"], []);
 };
@@ -48,9 +50,14 @@ function selectSiteQuery(params){
   // Prends en paramètre les parametres de l'url recue
   // Retourne un query objet que la biliotheque pg acceptera 
 
+  let SelectFields = 'SELECT '
+  SelectFields += 'uuid_site, code, nom, statut, communes, milieux_naturels, bassin_agence, prem_ctr, fin, responsable, validite, typ_site, typ_site_txt '
+  let FromTable = 'FROM sitcenca.listesitescenca ';
+  
   let query = {
     // text: 'SELECT uuid_site, code, prem_ctr, typ_site, typ_site_txt, milieux_naturels, communes, insee, bassin_agence, responsable, nom, espace, statut, fin, alerte_fin, validite
-    text: 'SELECT uuid_site, code, nom, statut, communes, milieux_naturels, bassin_agence, prem_ctr, fin, responsable, validite, typ_site, typ_site_txt FROM sitcenca.listesitescenca',
+    //text: 'SELECT uuid_site, code, nom, statut, communes, milieux_naturels, bassin_agence, prem_ctr, fin, responsable, validite, typ_site, typ_site_txt FROM sitcenca.listesitescenca',
+    text: joinQuery(SelectFields, FromTable, ''),
     values: [],
     // rowMode: 'array',
   };
@@ -144,6 +151,7 @@ async function run() {
       });
     });
     
+    // Données d'un site par son uuid
     app.get('/sites/uuid=:uuid', (req, res) => {
       const UUID = req.params.uuid;
       // let json = JSON.stringify({});
@@ -155,12 +163,12 @@ async function run() {
       SelectFields += 'espa.uuid_espace, espa.date_crea as date_crea_espace, espa.id_espace, espa.nom, espa.surface, espa.carto_hab, espa.zh, espa.typ_espace, espa.bassin_agence, espa.rgpt, espa.typ_geologie, '
       SelectFields += 'espa.id_source, espa.id_crea, espa.url, espa.maj_admin ';
 
-      let FromTable = 'FROM esp.geometries as geo ';
-      FromTable += 'LEFT JOIN esp.typ_geomnatures geona ON geo.typ_nature = geona.cd_type ';
-      FromTable += 'LEFT JOIN esp.espaces as espa ON geo.espace = espa.uuid_espace ';
+      let FromTable = 'FROM esp.espaces as espa ';
+      FromTable += 'LEFT JOIN esp.typ_espaces as typesp ON espa.typ_espace = typesp.cd_type ';
       FromTable += 'LEFT JOIN sitcenca.sites as site ON espa.uuid_espace = site.espace ';
       FromTable += 'LEFT JOIN sitcenca.typ_sites as tsite ON site.typ_site = tsite.cd_type ';
-      FromTable += 'LEFT JOIN esp.typ_espaces as typesp ON espa.typ_espace = typesp.cd_type ';
+      FromTable += 'LEFT JOIN esp.geometries as geo ON geo.espace = espa.uuid_espace ';
+      FromTable += 'LEFT JOIN esp.typ_geomnatures geona ON geo.typ_nature = geona.cd_type ';
       const where = 'where uuid_site = $1';
 
       const queryObject = {
@@ -180,12 +188,112 @@ async function run() {
           console.log("message : " + message);
         }else{
           let message = "erreur, la requête s'est mal exécutée. ";
-          res.end(message);
+          console.log(queryObject.text);
+          console.log(queryObject.values[UUID]);
           console.log(message);
+          res.end('');
         }
       });
     });
-    
+
+    // Communes
+    app.get('/sites/commune/uuid=:uuid', (req, res) => {
+      let SelectFields = 'SELECT '
+      // SelectFields += 'commune as insee, com.nom_officiel '
+      SelectFields += 'commune as insee, com.nom '
+      let FromTable = 'FROM esp.localisations loca ';
+      FromTable += 'left join esp.espaces espa on loca.espace = espa.uuid_espace ';
+      FromTable += 'left join sitcenca.sites site on espa.uuid_espace = site.espace ';
+      //FromTable += 'left join bd_topo.commune com on loca.commune = com.code_insee ';
+      FromTable += 'left join terr.listecommunes com on loca.commune = com.insee_com ';
+      const where = 'where site.uuid_site = $1 order by com.nom';
+
+      const UUID = req.params.uuid;
+      const queryObject = {
+        text: joinQuery(SelectFields, FromTable, where),
+        values: [UUID]
+      };
+
+      siteResearch(pool, {query: queryObject, "message": "sites/pgestion/uuid"}, 
+      (message, resultats) => {
+        if (resultats.length > 0) {
+          const json = JSON.stringify(resultats);
+          // console.log(json);
+          res.setHeader('Access-Control-Allow-Origin', '*');
+          res.setHeader('Content-Type', 'application/json; charset=utf-8');
+          res.end(json);
+        }else{
+          const json = JSON.stringify([]);
+          res.setHeader('Access-Control-Allow-Origin', '*');
+          res.setHeader('Content-Type', 'application/json; charset=utf-8');
+          res.end(json);
+        }
+      });
+    });
+
+    // Plans de gestion
+    app.get('/sites/pgestion/uuid=:uuid', (req, res) => {
+      let SelectFields = 'SELECT '
+      SelectFields += 'uuid_doc, document, annee_deb, annee_fin, docactuel, url, surface '
+      let FromTable = 'FROM docplan.docplanifsites ';
+      const where = 'where uuid_site = $1 order by docactuel';
+
+      const UUID = req.params.uuid;
+      const queryObject = {
+        text: joinQuery(SelectFields, FromTable, where),
+        values: [UUID]
+      };
+
+      siteResearch(pool, {query: queryObject, "message": "sites/pgestion/uuid"}, 
+      (message, resultats) => {
+        if (resultats.length > 0) {
+          const json = JSON.stringify(resultats);
+          // console.log(json);
+          res.setHeader('Access-Control-Allow-Origin', '*');
+          res.setHeader('Content-Type', 'application/json; charset=utf-8');
+          res.end(json);
+        }else{
+          const json = JSON.stringify([]);
+          res.setHeader('Access-Control-Allow-Origin', '*');
+          res.setHeader('Content-Type', 'application/json; charset=utf-8');
+          res.end(json);
+        }
+      });
+    });
+
+    // Milieux naturels
+    app.get('/sites/milnat/uuid=:uuid', (req, res) => {
+      const SelectFields = 'SELECT typmilnat.libelle as "type_milieu", milnat.pourcentage '
+      let FromTable = 'FROM esp.espaces espa ';
+      FromTable += 'left join sitcenca.sites site on espa.uuid_espace = site.espace ';
+      FromTable += 'left join esp.milieux_naturels milnat on espa.uuid_espace = milnat.espace ';
+      FromTable += 'left join esp.typ_milieuxnat typmilnat on milnat.typ_milieu = typmilnat.cd_type ';
+      const where = 'where site.uuid_site = $1 order by pourcentage desc';
+
+      const UUID = req.params.uuid;
+      const queryObject = {
+        text: joinQuery(SelectFields, FromTable, where),
+        values: [UUID]
+      };
+
+      siteResearch(pool, {query: queryObject, "message": "sites/milnat/uuid"}, 
+      (message, resultats) => {
+        if (resultats.length > 0) {
+          const json = JSON.stringify(resultats);
+          // console.log(json);
+          res.setHeader('Access-Control-Allow-Origin', '*');
+          res.setHeader('Content-Type', 'application/json; charset=utf-8');
+          res.end(json);
+        }else{
+          const json = JSON.stringify([]);
+          res.setHeader('Access-Control-Allow-Origin', '*');
+          res.setHeader('Content-Type', 'application/json; charset=utf-8');
+          res.end(json);
+        }
+      });
+    });
+
+    // Selectors de la barre de recherche de sites par critères
     app.get('/sites/selectors', (req, res) => {
       distinctSiteResearch(pool, [], "milieux_naturels",
         (selectors) => {
